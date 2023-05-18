20123e5399b3c00830f5484ed080dc04383046f1
kalnitsky80@gmail.com <kalnitsky80@gmail.com>
2023-05-18 09:30:41 +0300

    Initial Commit

---
+++ InetMagFG/InetMagFGApp.swift
@@ -0,0 +1,17 @@
+//
+//  InetMagFGApp.swift
+//  InetMagFG
+//
+//  Created by Dmitry Kalnitsky on 18/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct InetMagFGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
