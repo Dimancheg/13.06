d380b85fe1d7ba589103f8a3a56fa6190849c747
kalnitsky80@gmail.com <kalnitsky80@gmail.com>
2023-05-18 10:36:04 +0300

    авторизация

--- InetMagFG/ContentView.swift
+++ InetMagFG/ContentView.swift
@@ -8,14 +8,75 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var email = ""
+    @State private var pass = ""
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        VStack (spacing: 40){
+            Text("Авторизация")
+                .padding()
+                .padding(.horizontal, 40)
+                .font(.title2.bold())
+                .background(Color("whiteOp"))
+                .cornerRadius(20)
+            VStack{
+                TextField("Введите email...", text: $email)
+                    .padding()
+                    .background(Color("whiteOp"))
+                    .cornerRadius(10)
+                    .padding(8)
+                    .padding(.horizontal)
+                SecureField("Введите пароль...", text: $pass)
+                    .padding()
+                    .background(Color("whiteOp"))
+                    .cornerRadius(10)
+                    .padding(8)
+                    .padding(.horizontal)
+                Button{
+                    print("Авторизация")
+                } label: {
+                    Text("Войти")
+                        .padding()
+                        .padding(.horizontal, 40)
+                        .frame(maxWidth: .infinity)
+                        .background(LinearGradient(gradient: Gradient(colors: [ .indigo, .red]), startPoint: .leading, endPoint: .trailing))
+                        .font(.title2.bold())
+                        .cornerRadius(20)
+                        .padding(8)
+                        .padding(.horizontal)
+                        .foregroundColor(.white)
+                    
+                }
+                Button{
+                    print("Нет аккаунта")
+                } label: {
+                    Text("Регистрация")
+                        .padding()
+                        .padding(.horizontal, 40)
+                        .frame(maxWidth: .infinity)
+                        .font(.title2.bold())
+                        .cornerRadius(20)
+                        .padding(8)
+                        .padding(.horizontal)
+                        .foregroundColor(.white)
+                    
+                }
+                
+            }
+            .padding(.vertical)
+            .background(Color("whiteOp"))
+            .cornerRadius(10)
+            .padding()
+            
+            
+        }.frame(maxWidth: .infinity, maxHeight: .infinity)
+        
+            .background(Image("fon"))
+            .ignoresSafeArea()
+        
+        
     }
 }
 
